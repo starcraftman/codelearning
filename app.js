d21d06a64358fd2dc5ed964c229512b1527df3c6
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-07-23 08:30:44 -0400

    Moved to root dir path.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const rootDir = require('./util/path');
 
 const app = express();
 
@@ -10,9 +13,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).send(`
-<h1>Page not found</h1>
-`);
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
