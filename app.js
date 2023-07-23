3549bc8f2fb792d089f4d0b76d45dbf2b9979774
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-07-23 07:46:08 -0400

    Check in point.

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
-});
-
-app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>The "Add Product" Page</h1>');
-});
-
-app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).send(`
+<h1>Page not found</h1>
+`);
 });
 
 app.listen(3000);
