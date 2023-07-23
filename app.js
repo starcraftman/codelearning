4e56ffe0972a4251738350fffa36ce8372047add
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-07-23 13:58:09 -0400

    Snapshot end section 5

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const path = require('path');
-
 const express = require('express');
-const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const rootDir = require('./util/path');
-
+const rootDir = process.mainModule.filename;
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use(express.static(path.join(rootDir, 'public')));
+app.get('/users', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'users.html'));
+});
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'index.html'));
+});
+
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
