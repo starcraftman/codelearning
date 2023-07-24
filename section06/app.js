4b7bb990c126612c628c749dcbf0c83898b32321
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-07-24 15:09:40 -0400

    Ejs tour finished.

--- section06/app.js
+++ section06/app.js
@@ -6,12 +6,7 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs({
-    layoutsDir: 'views/layouts',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -24,7 +19,10 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found',
+        path: '404'
+    });
 });
 
 const port = 3000
