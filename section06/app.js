dce76bc93cee15aa5ee6dde2c18bc6871a136119
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-07-24 14:24:30 -0400

    Finished handlebar tour.

--- section06/app.js
+++ section06/app.js
@@ -2,10 +2,16 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs({
+    layoutsDir: 'views/layouts',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -21,4 +27,6 @@ app.use((req, res, next) => {
     res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
-app.listen(3000);
+const port = 3000
+console.log(`Serving on: localhost:${port}`);
+app.listen(port);
