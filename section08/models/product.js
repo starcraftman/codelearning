3184f833bf4f58fba21ff6b0024250bd129f7628
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-07-25 10:07:03 -0400

    Initial commit of section.

---
+++ section08/models/product.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../util/path');
+const productsFile = path.join(rootDir, 'data', 'products.json');
+const getProductsFromFile = (cb) => {
+    fs.readFile(productsFile, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else{
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
+
+module.exports = class Product {
+    constructor(t) {
+        this.title = t;
+    }
+
+    save() {
+        getProductsFromFile((products) => {
+            console.log(products);
+            products.push(this);
+            fs.writeFile(productsFile, JSON.stringify(products), (err) => console.log(err));
+        });
+    }
+
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
+    }
+}
---
+++ section08/routes/shop.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
+
+const router = express.Router();
+
+router.get('/', productsController.getProducts);
+
+module.exports = router;
