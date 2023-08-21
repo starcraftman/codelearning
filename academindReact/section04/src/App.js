2890f07d2655a154741707757ac1313f9c49ea75
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-08-20 22:08:55 -0400

    extract header

--- academindReact/section04/src/App.js
+++ academindReact/section04/src/App.js
@@ -1,8 +1,9 @@
-import keyConceptsImage from "./assets/images/key-concepts.png";
 import componentsImage from "./assets/images/components.png";
 import stateImage from "./assets/images/state.png";
 import eventsImage from "./assets/images/events.png";
+
 import Concepts from "./comps/Concepts";
+import Header from "./comps/Header";
 
 const concepts = [
   {
@@ -28,11 +29,7 @@ const concepts = [
 function App() {
   return (
     <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
+      <Header />
       <Concepts concepts={concepts} />
     </div>
   );
