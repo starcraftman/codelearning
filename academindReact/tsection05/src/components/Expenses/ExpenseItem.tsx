0b69a859b51f803bd5ed2eb0590061af108dfa58
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-08-21 13:20:21 -0400

    cleanup

--- academindReact/tsection05/src/components/Expenses/ExpenseItem.tsx
+++ academindReact/tsection05/src/components/Expenses/ExpenseItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Card from "./../UI/Card"
 import ExpenseDate from "./ExpenseDate";
@@ -12,20 +12,13 @@ export interface PropsType {
 };
 
 const ExpenseItem = (props: PropsType) => {
-  const [title, setTitle] = useState(props.title)
-
-  const clickHandler = () => {
-    setTitle("Updated");
-    console.log("Updated the title")
-  }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
-        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
