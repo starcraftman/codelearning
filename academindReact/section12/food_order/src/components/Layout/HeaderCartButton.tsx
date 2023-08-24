f2b7091f3d1274f24144cfe202b80f3bfc3f2fc9
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-08-24 17:50:38 -0400

    end 183

--- academindReact/section12/food_order/src/components/Layout/HeaderCartButton.tsx
+++ academindReact/section12/food_order/src/components/Layout/HeaderCartButton.tsx
@@ -9,9 +9,26 @@ interface PropsType {
 }
 
 const HeaderCartButton = (props: PropsType) => {
+  const [animateButton, setAnimateButton] = React.useState(false);
   const cartCtx = React.useContext(CartContext);
+  const btnClasses = `${styles.button} ${animateButton ? styles.bump : ""}`;
+  React.useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+
+    setAnimateButton(true);
+    const timeout = setTimeout(() => {
+      setAnimateButton(false);
+    }, 300)
+
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [cartCtx.items]);
+
   return (
-    <button onClick={props.onClick} className={styles.button}>
+    <button onClick={props.onClick} className={btnClasses}>
         <span className={styles.icon}>
             <CartIcon />
         </span>
