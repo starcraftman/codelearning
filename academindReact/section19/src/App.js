981df8710083fcf36bd951fe716f72cb7fe9f715
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-08-27 14:34:01 -0400

    unused import

--- academindReact/section19/src/App.js
+++ academindReact/section19/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import Auth from "./components/Auth";
 import Counter from './components/Counter';
