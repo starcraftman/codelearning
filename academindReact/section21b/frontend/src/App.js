c5c457ab60351cb5a4ee092d2b72230e86809bf1
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-08-28 18:06:34 -0400

    end 320

--- academindReact/section21b/frontend/src/App.js
+++ academindReact/section21b/frontend/src/App.js
@@ -23,6 +23,16 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <EventsPage />,
+            loader: async () => {
+              const response = await fetch('http://localhost:8080/events');
+
+              if (!response.ok) {
+                // TODO
+              } else {
+                const resData = await response.json()
+                return resData.events;
+              }
+            }
           },
           {
             path: "new",
--- academindReact/section21b/frontend/src/pages/Events.js
+++ academindReact/section21b/frontend/src/pages/Events.js
@@ -1,14 +1,9 @@
-import React from "react";
-import EventsList from "../components/EventsList";
+import EventsList from '../components/EventsList';
 
-const DUMMY_EVENTS = [
-  { id: "e1", title: "Event 1", image: null, date: "10:00" },
-  { id: "e2", title: "Event 2", image: null, date: "12:00" },
-  { id: "e3", title: "Event 3", image: null, date: "15:00" },
-  { id: "e4", title: "Event 4", image: null, date: "17:00" },
-];
 function EventsPage() {
-  return <EventsList events={DUMMY_EVENTS} />;
+  return (
+    <EventsList />
+  );
 }
 
 export default EventsPage;
