e54994e38bfff095e3ab22d10ab091fc42b6cde8
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-09-05 10:48:49 -0400

    end 370

---
+++ academindReact/section24/src/components/Events/NewEventsSection.jsx
@@ -0,0 +1,48 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvents } from "../../util/http.js";
+
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
+
+export default function NewEventsSection() {
+  const query = useQuery({
+    queryKey: ["events"],
+    queryFn: fetchEvents,
+  });
+  let content;
+
+  if (query.isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (query.isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurred"
+        message={query.error.info?.message || "Failed to fetch events"}
+      />
+    );
+  }
+
+  if (query.data) {
+    content = (
+      <ul className="events-list">
+        {query.data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <section className="content-section" id="new-events-section">
+      <header>
+        <h2>Recently added events</h2>
+      </header>
+      {content}
+    </section>
+  );
+}
