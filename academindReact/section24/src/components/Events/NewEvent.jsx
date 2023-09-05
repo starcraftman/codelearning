c04b3c1b4140d578fc97862c335576b07601e1de
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-09-05 14:32:25 -0400

    375

--- academindReact/section24/src/components/Events/NewEvent.jsx
+++ academindReact/section24/src/components/Events/NewEvent.jsx
@@ -1,25 +1,39 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { createNewEvent } from "../../util/http";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import ErrorBlock from "../UI/ErrorBlock";
 
 export default function NewEvent() {
   const navigate = useNavigate();
+  const query = useMutation({
+    mutationFn: createNewEvent,
+  });
 
-  function handleSubmit(formData) {}
+  function handleSubmit(formData) {
+    query.mutate({ event: formData });
+  }
+
+  const formContent = (
+    <>
+      <Link to="../" className="button-text">
+        Cancel
+      </Link>
+      <button type="submit" className="button">
+        Create
+      </button>
+    </>
+  );
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {query.isPending && "Submitting ..."}
+        {!query.isPending && formContent}
       </EventForm>
+      {query.isError && <ErrorBlock title="Failed to create event" message={query.error.info?.message || "Failed to create event. Please check and try again later."} />}
     </Modal>
   );
 }
--- academindReact/section24/src/util/http.js
+++ academindReact/section24/src/util/http.js
@@ -1,17 +1,37 @@
 const URL = `http://localhost:3000/events`;
 
 export async function fetchEvents({ signal, searchTerm }) {
-    const suffix = searchTerm ? `?search=${searchTerm}` : "";
-    const response = await fetch(URL + suffix, {signal: signal});
+  const suffix = searchTerm ? `?search=${searchTerm}` : "";
+  const response = await fetch(URL + suffix, { signal: signal });
 
-    if (!response.ok) {
-      const error = new Error('An error occurred while fetching the events');
-      error.code = response.status;
-      error.info = await response.json();
-      throw error;
-    }
-
-    const { events } = await response.json();
-
-    return events;
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the events");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
   }
+
+  const { events } = await response.json();
+
+  return events;
+}
+
+export async function createNewEvent(eventData) {
+  const response = await fetch(URL, {
+    method: "POST",
+    body: JSON.stringify(eventData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the events");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+  return event;
+}
