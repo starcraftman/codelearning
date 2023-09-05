f181f1c788ac59ecf67d08738d9a6481214b9b76
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-09-05 09:43:33 -0400

    end 416

--- academindReact/section25b/pages/[meetupId]/index.js
+++ academindReact/section25b/pages/[meetupId]/index.js
@@ -1,24 +1,32 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
+import { getDbClient } from "../../util/secrets";
+import { ObjectId } from "mongodb";
 
-const MeetupDetails = () => {
-  const meetup = {
-    id: "m1",
-    title: "First Meetup",
-    image:
-      "https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638884690/EducationHub/photos/bengali-village.jpg",
-    address: "Some address, 1235 Some City",
-    description: "This is a first meetup",
-  };
-  return <MeetupDetail {...meetup} />;
+const MeetupDetails = (props) => {
+  console.log(props.meetup);
+  return <MeetupDetail {...props.meetup} />;
 };
 
 export async function getStaticPaths() {
-  const paths = ["m1", "m2", "m3"].map(x => { 
-    return {params: { meetupId: x }}
-  });
-  return {
-    paths: paths,
-    fallback: false
+  const client = getDbClient();
+  try {
+      await client.connect();
+      const meetupIds = await client
+        .db()
+        .collection("meetups")
+        .find({}, {_id: 1})
+        .toArray();
+
+      const paths = meetupIds.map(x => { 
+        return {params: { meetupId: x._id.toString() }}
+      });
+      return {
+        paths: paths,
+        fallback: false
+      }
+
+  } finally {
+      client.close();
   }
 }
 
@@ -28,12 +36,27 @@ export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
 
   // Use backend to fetch data for static build
+  const client = getDbClient();
+  try {
+      await client.connect();
+      const meetup = await client
+        .db()
+        .collection("meetups")
+        .findOne({_id: new ObjectId(meetupId)});
 
-  return {
-      props: {
-          meetup: {} 
-      },
-      revalidate: 10
+        return {
+          props: {
+              meetup: {
+                ...meetup,
+                _id: meetup._id.toString(),
+                id: meetup._id.toString()
+              } 
+          },
+          revalidate: 10
+      }
+
+  } finally {
+      client.close();
   }
 };
 
