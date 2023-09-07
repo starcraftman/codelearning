565e4e5eb17b1b77b02020ac29fb6ec9ad4a26cf
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-09-07 19:14:52 -0400

    Finished section04

---
+++ flutter/section04/android/app/src/main/kotlin/com/example/section03/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.section03
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/section04/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:section03/quiz.dart';
+
+void main() {
+  runApp(const Quiz());
+}
