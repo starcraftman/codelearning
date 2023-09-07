91a08843001561a602ad607cdeb7c1f07041dfd0
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-09-07 11:36:11 -0400

    74

--- flutter/section03/lib/models/quiz_question.dart
+++ flutter/section03/lib/models/quiz_question.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
   final List<String> choices;
 
   const QuizQuestion(this.question, this.choices);
+
+  List<String> getShuffledChoices() {
+    final shuffledChoices = List.of(choices);
+    shuffledChoices.shuffle();
+    return shuffledChoices;
+  }
 }
--- flutter/section03/lib/questions_screen.dart
+++ flutter/section03/lib/questions_screen.dart
@@ -14,34 +14,31 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
-  String question = "How many legs on my cat?";
-  List<String> choices = ["2 legs", "3 legs", "4 legs", "5 legs"];
-  String answer = "4 legs";
-  int index = 0;
+  int questionIndex = 0;
 
-  void setQuestion({required question, required choices, required answer}) {
-    this.question = question;
-    this.choices = choices;
-    this.answer = answer;
-  }
-
-  void verify(int choice) {
-    if (choices[choice] == answer) {
-      print("Correct answer");
+  void answerQuestion(String choice, String answer) {
+    if (answer == choice) {
+      print("Correct");
     } else {
-      print("Wrong!!!");
+      print("Wrong");
+    }
+    if (questions.length != questionIndex + 1) {
+      setState(() {
+        questionIndex += 1;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[questionIndex];
 
-    final shuffledChoices = [...currentQuestion.choices];
     // Mapped then expand 2d list
-    var answersWithSizes = shuffledChoices.map((choice) {
+    var answersWithSizes = currentQuestion.getShuffledChoices().map((choice) {
       return [
-        AnswerButton(choice: choice, onPressHandler: () {}),
+        AnswerButton(choice: choice, onPressHandler: () {
+          answerQuestion(choice, currentQuestion.choices[0]);
+        }),
         const SizedBox(height: 12)
       ];
     });
@@ -70,14 +67,14 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text(
-                currentQuestion.question,
+            Text(currentQuestion.question,
                 style: const TextStyle(fontSize: 18, color: Colors.white),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 30),
-              ...colChildren
-            ]),
+              textAlign: TextAlign.center,
+
+            ),
+            const SizedBox(height: 30),
+          ...colChildren
+        ]),
       ),
     );
   }
