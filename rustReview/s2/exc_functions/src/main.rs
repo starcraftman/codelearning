fc6fa68009db0a48ad07a89a67f1b14550ea9729
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2023-09-20 11:07:22 -0400

    add rust refresh

---
+++ rustReview/s2/exc_functions/src/main.rs
@@ -0,0 +1,22 @@
+// Silence some warnings so they don't distract from the exercise.
+#![allow(unused_variables)]
+
+fn main() {
+    let width = 4;
+    let height = 7;
+    let depth = 10;
+    {
+        let area = area_of(width, height);
+        println!("Area is {}", area);
+    }
+
+    println!("Volume is {}", volume(width, height, depth));
+}
+
+fn area_of(x: i32, y: i32) -> i32 {
+    x * y
+}
+
+fn volume(w: i32, h: i32, d: i32) -> i32 {
+    w * h * d
+}
---
+++ rustReview/s2/variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let bunnies: i32 = 2;
+    // print!("%d", bunnies);
+    let mut hares = 33;
+    const WARP_FACTOR: f64 = 9.9;
+    println!("{bunnies} {hares}, {WARP_FACTOR}");
+    hares = 22;
+    println!("{bunnies} {hares}, {WARP_FACTOR}");
+}
